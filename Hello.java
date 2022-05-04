02092a53a592ec39305944220193f0b1c8590e75
Madhuura <101631345+Madhuura@users.noreply.github.com>
2022-05-04 23:45:40 +0530

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,9 @@
+import java.lang.*;
+import java.util.*;
+
+public class Hello{
+public static void main(String[] args) {
+System.out.println("Hello, World!\nThis is Jenkins");
+System.out.println("Madhura Kulkarni\nCdac Noida");
+}
+}
